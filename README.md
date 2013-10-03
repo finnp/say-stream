481406b9b53a0864e96a0de82190c139a508c22e
Finn <derfinn@gmail.com>
2013-10-03 20:27:43 +0200

    yay

---
+++ README.md
@@ -0,0 +1,29 @@
+### Examples
+
+ ```javascript
+ // A robot speaking everything from stdin
+var say = require('say-stream');
+var robot = say();
+process.stdin.pipe(robot);
+ ```
+
+ ```javascript
+var http = require('http');
+var choir = say("cellos");
+var url = "http://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new";
+http.get(url, function(request) {
+ 	request.setEncoding('utf8');
+ 	request.pipe(choir).pipe(process.stdout);
+});
+```
+
+
+ ```javascript
+var net = require('net');
+var whisper = say('whisper');
+var server = net.createServer(function(socket) {
+	socket.pipe(whisper).pipe(process.stdout);
+});
+server.listen(8000);
+console.log("Listening on port 8000");
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+
+var Stream = require('stream');
+var exec = require('child_process').exec;
+
+module.exports = function(voice) {
+
+	voice = voice || "Zarvox";
+
+	var say = exec("trap 'exit' INT; while read line; do echo $line; echo $line | say -v " + voice + " ; done" ,
+	  function (error, stdout, stderr) {
+	    if (error !== null) {
+	      console.log('exec error: ' + error);
+	    }
+	});
+
+	var stream = new Stream();
+	stream.readable = stream.writable = true
+
+	stream.write = function(data) {
+		say.stdin.write(data.toString() + "\n");
+		return true;
+	};
+
+	stream.end = function(data) {
+		say.stdin.end();
+	};
+
+	say.stdout.on('data', function(data) {
+	 	stream.emit('data', data);
+	});
+	say.stdout.on('end', function() {
+	 	stream.emit('end');
+	});
+
+	return stream;
+};
