71be50a04ac3c1eb8c11f357209c198f91660347
Finn <derfinn@gmail.com>
2014-04-22 22:53:59 +0200

    Extend from Commandliner

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var util = require('util');
 var Transform = require('stream').Transform;
 var exec = require('child_process').exec;
-var cmdliner = require('commandliner');
+var Commandliner = require('commandliner');
+var extend = require('extend');
 
 function Say (voice, options) {
   Transform.call(this, options);
-  this.voice = voice || 'Zarvox';
+  Commandliner.call(this, 'say')
+  this.options = {
+    'interactive' : '\"\"',
+    'voice': voice
+  }
 };
 
 util.inherits(Say, Transform);
+extend(true, Say.prototype, Commandliner.prototype);
 
 Say.prototype._transform = function(data, encoding, done) {
-  var text = data.toString();
-  var saycmd = cmdliner('say', {'interactive' : '""', 'voice': this.voice}, text);
-  var child = exec(saycmd);
+  this.args = data.toString();
+  var child = exec(this);
   child.stderr.pipe(process.stderr);
   child.stdout.on('data', this.push.bind(this));
   child.stdout.on('end', done);
